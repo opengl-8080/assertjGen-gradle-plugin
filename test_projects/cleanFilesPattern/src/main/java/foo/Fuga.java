752a0975e0f2503fc52c3d72ff9ffaf973404ecf
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-09-13 21:26:59 +0900

    #15 add cleanFilePatterns project

---
+++ test_projects/cleanFilesPattern/src/main/java/foo/Fuga.java
@@ -0,0 +1,30 @@
+package foo;
+
+public class Fuga {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Fuga{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
